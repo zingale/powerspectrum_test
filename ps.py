fix error pointed out by Chris in ky normalization (doesn't matter for cubes)

ps.py
@@ -83,7 +83,7 @@ kx = kx*N/L[0]
 
 ky = np.fft.fftfreq(N)[0:N/2+1]
 ky[-1] *= -1
-ky = ky*N/L[2]
+ky = ky*N/L[1]
 
 kz = np.fft.fftfreq(N)[0:N/2+1]
 kz[-1] *= -1